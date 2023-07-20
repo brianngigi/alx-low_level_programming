1c38b52600764ce15363fefc1f901ffd97a967be
Brian <briannganga6072@gmail.com>
2023-07-20 04:57:19 -0700

    most

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,7 +4,7 @@
  * mul - Multiplies two intergers
  * @a: The first number to multiply
  * @b: The second number to be multiplied by the first one
- * Return: The result of multiplying the a param and b  param
+ * Return: The result of multiplying the a param and b param
  */
 int mul(int a, int b)
 {
