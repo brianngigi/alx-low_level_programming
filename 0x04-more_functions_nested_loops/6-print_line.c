cf45aab4d0b8fb2934af80f5ca94386f96c7f33d
Brian <briannganga6072@gmail.com>
2023-07-20 07:53:45 -0700

    Prints line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,19 +2,22 @@
 
 /**
  * print_line - Draws a straight line according to parameter
- * @n: The number of lines to draw 
+ * @n: The number of lines to draw
  * Return: empty
  */
 
-int main(void)
+void print_line(int n)
 {
 	int x;
-	if(n <= 0)
+
+	if (n <= 0)
+
 	{
 		_putchar('\n');
 	}
 	else
-	{for (x = 0; x < n; x++)
+	{
+		for (x = 0; x < n; x++)
 		{
 			_putchar(95);
 		}
